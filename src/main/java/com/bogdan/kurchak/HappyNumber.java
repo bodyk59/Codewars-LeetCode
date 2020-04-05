aa91da7ab592e44641caf16bcd3d41c6080f513a
Kurchak Bogdan <bogdan.kurchak@icloud.com>
2020-04-05 18:45:32 +0300

    Added HappyNumber.java

---
+++ src/main/java/com/bogdan/kurchak/HappyNumber.java
@@ -0,0 +1,42 @@
+package com.bogdan.kurchak;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Link: https://leetcode.com/problems/happy-number/
+ *
+ * Task: Write an algorithm to determine if a number is "happy".
+ * A happy number is a number defined by the following process:
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits,
+ * and repeat the process until the number equals 1 (where it will stay),
+ * or it loops endlessly in a cycle which does not include 1.
+ * Those numbers for which this process ends in 1 are happy numbers.
+ */
+
+/**
+ * @author Bogdan Kurchak
+ */
+public class HappyNumber {
+    private int nextNumber(int number) {
+        int sum = 0;
+
+        while (number != 0) {
+            sum += (int) Math.pow(number % 10, 2);
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> numberSet = new HashSet<>();
+
+        while (n != 1 && !numberSet.contains(n)) {
+            numberSet.add(n);
+            n = nextNumber(n);
+        }
+
+        return n == 1;
+    }
+}
